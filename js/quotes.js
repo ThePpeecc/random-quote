0fa9f9736791845327490d88ecd0ec675ff93f00
ThePpeecc <Marcush@live.dk>
2016-09-09 17:53:36 +0200

    First Full Version
    This commit should hold a finished random quote generator, with all the
    necessary functionality to pass

--- js/quotes.js
+++ js/quotes.js
@@ -1,5 +1,9 @@
-//Holds all the qoutes 
+//Holds all the qoutes
 var quotes = [
-  {
-    quote: "Stuff", source: "Stuff", citation: "Citation", year: "year",}
+  {quote: "You can do anything but not everything", source: "David Allen", citation: "Making It All Work", year: 2009, tags: "inspiration"},
+  {quote: "The more laws, the less justice", source: "Marcus Tullius Cicero", citation: "Roman Statesman", tags: "law"},
+  {quote: "Do not pray for easy lives. Pray to be stronger men", source: "John F. Kennedy", citation: "Remarks at Presidential Prayer Breakfast", year: 1963, tags: "politics"},
+  {quote: "The whole purpose of education is to turn mirrors into windows.", source: "Sydney Harris", citation: "New York Public Library Student Planner", year: 2006, tags: "education"},
+  {quote: "Educating the mind without educating the heart is no education at all", source: "Aristotle", citation: "Philosopher", tags: "education"},
+  {quote: "We can’t help everyone, but everyone can help someone", source: "Ronald Reagan", tags: "politics"}
 ];
--- js/script.js
+++ js/script.js
@@ -1,3 +1,61 @@
-// event listener to respond to "Show another quote" button clicks
-// when user clicks anywhere on the button, the "printQuote" function is called
+//Gets the different places to print out the qoute over
+var quoteDisplay = document.getElementsByClassName("quote");
+var sourceDisplay = document.getElementsByClassName("source");
+
+//We want to change the quote every 30 seconds
+let timerLength = 30000;
+var timer;
+
+//This variable holds a copy of all the quotes from the 'database'
+var quotesCopy = [ ];
+
+//Create a random index
+function randomNumber(maxIndex){
+  return Math.floor(Math.random()*maxIndex);
+}
+
+function setTimer() {
+  //We clear the timer incase the user pushed the button and set a new timer
+  clearTimeout(timer);
+  timer = setTimeout(printQuote, timerLength);
+}
+
+function randomRGBColor(max){
+  return 'rgb('+ randomNumber(max) +', '+ randomNumber(max) +','+ randomNumber(max) +')';
+}
+
+//Loads a random quote from the quotes.js files
+function getRandomQuote(){
+  //in case we have run out of quotes, get a new batch of quotes from the 'datebase'
+  if (quotesCopy.length <= 0){ quotesCopy = quotesCopy.concat(quotes); }
+  //returns quote while removeing it from the array
+  return quotesCopy.splice(randomNumber(quotesCopy.length), 1)[0];
+}
+
+var printQuote = function() {
+  //First we get a random quote
+  var rnQuote = getRandomQuote();
+
+  //Start of the HTMl that goes in the source paragraf
+  var sourceHTML = rnQuote.source;
+
+  //We tjek if we have a citation
+  if (rnQuote.hasOwnProperty("citation")) { sourceHTML += '<span class="citation">' + rnQuote.citation +'</span>'; }
+  //We tjek if we have a year date
+  if (rnQuote.hasOwnProperty("year")) { sourceHTML += '<span class="year">' + rnQuote.year +'</span>'; }
+  //We tjek for potential tags
+  if (rnQuote.hasOwnProperty("tags")) { sourceHTML += '<span class="tags">' + rnQuote.tags +'</span>'; }
+
+  //We add alle the html to the quote and source
+  quoteDisplay[0].innerHTML = rnQuote.quote;
+  sourceDisplay[0].innerHTML = sourceHTML;
+  //Change the background color, but at a max rgb value of 200 so we dont get a white page, and cant see the quote
+  document.body.style.backgroundColor = randomRGBColor(200);
+
+  //We set a new timer since the last one has stopped running now
+  setTimer();
+};
+
+//Here we add eventlisteners and set the timer
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+setTimer()
